6aa26dc63d822be7364f9243c25267741a1eb9f4
muhammedsafwan411786-lab <muhammedsafwan411786@gmail.com>
2025-11-14 23:58:18 +0530

    Configure STATIC_ROOT for Render

--- islamic_backend/settings.py
+++ islamic_backend/settings.py
@@ -127,3 +127,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+#This is for Render deployment.
+    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
